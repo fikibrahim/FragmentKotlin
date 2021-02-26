dabc645d2155b372cd9f9605a2bb1e6f9d08478a
fikibrahim <taufikibrahim21@gmail.com>
2021-02-26 15:27:01 +0700

    fragment

---
+++ app/src/main/java/com/taufikibrahim/fragmentrefactory/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.taufikibrahim.fragmentrefactory
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+
+    var isFragmentOneLoaded = true
+    val manager = supportFragmentManager
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val Change = findViewById<Button>(R.id.btn_changes)
+        ShowFragmentOne()
+
+        Change.setOnClickListener({if (isFragmentOneLoaded)
+        ShowFragmentTwo()
+        else
+        ShowFragmentOne()})
+    }
+
+    fun ShowFragmentOne(){
+        val transaction = manager.beginTransaction()
+        val fragment = FragmentOne()
+        transaction.replace(R.id.fragment_holder, fragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
+        isFragmentOneLoaded = true
+    }
+
+    fun ShowFragmentTwo(){
+        val transaction = manager.beginTransaction()
+        val fragment = FragmentTwo()
+        transaction.replace(R.id.fragment_holder, fragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
+        isFragmentOneLoaded = false
+    }
+}
